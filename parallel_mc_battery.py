f440b99d217fdfda1e69adc809dac91c8e415b9e
Vlad <rotariuvladimir@gmail.com>
2024-02-11 19:26:56 +0100

    Add validator for battery configs

--- parallel_mc_battery.py
+++ parallel_mc_battery.py
@@ -8,12 +8,35 @@ import apache_beam as beam
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-class Parallel_MC_Battery:
+class ParallelMCBattery:
     '''
     Helper class to orchestrate in parallel Monte Carlo simulations for an arbitrary number of models, 
     with low-level parameter granularity.
     '''
     def __init__(self, battery_configs, pipeline_options):
+        
+        class BatteryConfigs(BaseModel):
+            battery_configs: dict[str, str] #rng, mode (production, testing)
+
+            @validator("battery_configs")
+            def values_allowed_options(cls, configs):
+                allowed_rngs = ["PCG64", "Philox", "SFC64", "MT19937"]
+                allowed_modes = ["production", "testing"]
+
+                if configs["rng"] not in allowed_rngs:
+                    raise ValueError(f"The chosen RNG could not be found.\
+                                        Allowed options: {str(allowed_rngs)[1,-1]}")
+                
+                if configs["mode"] not in allowed_modes:
+                    raise ValueError(f"The chosen mode could not be found.\
+                                        Allowed options: {str(allowed_modes)[1,-1]}")
+                
+
+        try:        
+            battery_configs = BatteryConfigs(battery_configs)
+        except ValueError:
+            logging.exception("Validation of battery_configs failed")
+
         self.battery_configs = battery_configs
         self.pipeline_options = pipeline_options
         
@@ -22,11 +45,17 @@ class Parallel_MC_Battery:
             bundle_counter = 0
 
             def start_bundle(self):
-                logging.info(f"Bundle {type(self).bundle_counter} created, and sent to workers for parallel simulation...")
+                logging.info(f"Bundle {type(self).bundle_counter} created,\
+                              and sent to workers for parallel simulation...")
                 type(self).bundle_counter += 1
 
             def process(self, element):
                 model, simulation_configs, output_path= element
+
+                try:
+                    simulation_configs = self.__validate_simulation_configs(simulation_configs)
+                except ValueError:
+                    logging.exception("Validation of simulation_configs failed")
 
                 parameters = simulation_configs["parameters"]
                 starting_point = simulation_configs["starting_point"]
@@ -57,8 +86,13 @@ class Parallel_MC_Battery:
                     @validator("simulation_configs")
                     def values_math_relevant(cls, configs):
                         if configs["number_points"] < 1:
-                            raise ValueError("The minimum number of points in a single Monte Carlo trace should be >= 1.")
+                            raise ValueError("The minimum number of points in a single Monte Carlo\
+                                              trace should be >= 1.")
+                        
                         if configs["number_simulations"] < 1:
                             raise ValueError("The minimum number of Monte Carlo traces should be >= 1.")
                         return configs
 
+                return SimulationConfigs(simulation_configs)
+
+        
