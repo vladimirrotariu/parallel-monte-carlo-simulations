d5d0ddc6b01de87daafe3843982166124d68571a
Vlad <rotariuvladimir@gmail.com>
2024-02-11 10:47:06 +0100

    Implement parallel logic for simulations

--- parallel_mc_battery.py
+++ parallel_mc_battery.py
@@ -6,16 +6,26 @@ import apache_beam as beam
 
 class Parallel_MC_Battery:
     '''
-    Helper class to orchestrate in parallel Monte Carlo simulations for an arbitrary number of models.
+    Helper class to orchestrate in parallel Monte Carlo simulations for an arbitrary number of models, 
+    with low-level parameter granularity.
     '''
     def __init__(self, battery_configs, pipeline_options):
         self.battery_configs = battery_configs
         self.pipeline_options = pipeline_options
         
-    def simulate(self, model, parameters, number_simulations, output_path):
-        self.model = model
-        self.parameters = parameters
-        self.number_simulations = number_simulations
-        self.output_path = output_path
+    def simulate(self, models, parameters, starting_points, number_simulations, output_paths):
+        class SimulateDoFn(beam.DoFn):
+            def process(self, element):
+                model, individual_parameters, starting_point, number_individual_simulations, output_path = element
+
+                monte_carlo_point = starting_point
+                model 
+
+                monte_carlo_trace = [monte_carlo_point]
+                for _ in number_individual_simulations:
+                    monte_carlo_point = model(monte_carlo_point)
+                    monte_carlo_trace.append()
+                
+                yield monte_carlo_trace
 
 
